561ae4e4528abddc62fd871aa7e915ee9bce4f66
GermanPikel <germanpikel@gmail.com>
2026-02-14 19:32:32 +0300

    added logging with loguru

--- app/main.py
+++ app/main.py
@@ -1,12 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
+from loguru import logger
+from uuid import uuid4
+from fastapi.responses import JSONResponse
 
 from app.routers import categories, products, users, reviews, cart, orders
+
+logger.add("info.log", format="Log: [{extra[log_id]}:{time} - {level} - {message}]", level="INFO", enqueue=True)
 
 app = FastAPI(
     title="FastAPI интернет-магазин",
     version="0.1.0",
 )
+
+
+@app.middleware("http")
+async def log_middleware(request: Request, call_next):
+    log_id = str(uuid4())
+    with logger.contextualize(log_id=log_id):
+        try:
+            response = await call_next(request)
+            if response.status_code in [401, 402, 403, 404]:
+                logger.warning(f"Request to {request.url.path} failed")
+            else:
+                logger.info("Successfully accessed " + request.url.path)
+        except Exception as ex:
+            logger.error(f"Request to {request.url.path} failed: {ex}")
+            response = JSONResponse(content={"succcess": False}, status_code=500)
+        return response
+
 
 app.mount("/media", StaticFiles(directory="media"), name="media")
 
